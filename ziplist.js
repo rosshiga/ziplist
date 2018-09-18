592dff66c2fd193146e9c37125cbe1bf72ba4478
Ross Higa <ross@rosshiga.com>
2018-09-17 22:50:44 -1000

    finish

--- ziplist.js
+++ ziplist.js
@@ -2,21 +2,24 @@ const listA = ['a', 'b', 'c'];
 const listB = [1, 2, 3];
 
 function zipList(listin1, listin2) {
-  let array = [];
-  let list1 = listin1.slice();
-  let list2 = listin2.slice();
-  while (list1.length !== 0 | list2.length !==) {
+  const array = [];
+  const list1 = listin1.slice();
+  const list2 = listin2.slice();
+  while (list1.length !== 0 || list2.length !== 0) {
     if (list1.length !== 0) {
-      array.push(list1.pop());
+      array.push(list1.shift());
     }
     if (list2.length !== 0) {
-      array.push(list2.pop());
+      array.push(list2.shift());
     }
   }
 
   return array;
-
 }
 
+function zipListTheSimpleWay(list1, list2) {
+  return _.flatten(_.zip(list1, list2));
+}
 
 console.log(zipList(listA, listB));
+console.log(zipListTheSimpleWay(listA, listB));
